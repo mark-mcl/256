1ed439231ce0efe482ac958813b06af3a7113d11
Mark McLoughlin & Thuy <pair+mark-mcl.thuynt13@devbootcamp.com>
2016-10-10 16:31:00 -0500

    Write methods to handle merging in multiple directions

--- js/game.js
+++ js/game.js
@@ -3,20 +3,74 @@ function Game (gameBoard = "0000202000000000") {
 
 };
 
+function transpose(matrix){
+    var newMatrix = [];
+    for(var i = 0; i < matrix.length; i++){
+        newMatrix.push([]);
+    };
+    for(var i = 0; i < matrix.length; i++){
+        for(var j = 0; j < matrix.length; j++){
+            newMatrix[j].push(matrix[i][j]);
+        };
+    };
+    return(newMatrix);
+};
+
+Game.prototype.moveLeft = function() {
+  for (var row of this.boardArray) {
+    console.log(row);
+    for ( var i= 0; i < row.length; i++) {
+      row[i] = move_tile_left(row, i);
+    };
+  };
+};
+
+function move_tile_left(row, i) {
+  if (row[i] == "0" && (i != 3)) {
+    return move_tile_left(row, i+1);
+  } else {
+    var value = row[i];
+    row[i] = "0";
+    return value;
+  }
+};
+
+Game.prototype.mergeLeft = function() {
+  for (var row of this.boardArray) {
+    if (row[0] === row[1] && row[2] === row[3]) {
+      row[0] = (parseInt(row[0])*2).toString();
+      row[1] = (parseInt(row[3])*2).toString();
+      row[2] = "0";
+      row[3] = "0";
+    } else {
+      for ( var i= 0; i < row.length; i++) {
+        if (row[i] === row[i+1] && (i < row.length)) {
+          row[i+1] = "0";
+          row[i] = (parseInt(row[i])*2).toString();
+        } else if (row[i] === "0" && (i < row.length)) {
+          row[i] = row[i+1];
+          row[i+1] = "0";
+        };
+    };
+  };
+};
+};
+
+
 
 
 Game.prototype.moveRight = function() {
   for (var row of this.boardArray) {
     console.log(row);
     for ( var i= row.length-1; i > 0; i--) {
-      row[i] = move_tile(row, i);
+      row[i] = move_tile_right(row, i);
     };
   };
 };
 
-function move_tile(row, i) {
-  if (row[i] == "0" && (i != 0)) {
-    return move_tile(row, i-1);
+function move_tile_right(row, i) {
+  if (row[i] == "0" && (i != 3)) {
+    return move_tile_right(row, i-1);
   } else {
     var value = row[i];
     row[i] = "0";
